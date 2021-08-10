80f29c19a7ede8ebfc5c1ce929e9fa51d5fb43b5
velopace <velopace@outlook.com>
2021-08-10 14:21:40 -0500

    Create post in react

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,83 @@
-import logo from '../logo.png';
+import { useState, useEffect } from 'react';
+import Web3 from 'web3';
 import './App.css';
+import SocialNetwork from '../abis/SocialNetwork.json';
+import Navbar from './Navbar';
+import Main from './Main';
 
 function App() {
+
+  const [account, setAccount] = useState('');
+  const [socialNetwork, setSocialNetwork] = useState({});
+  const [postCount, setPostCount] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  const loadBlockchainData = async () => {
+    const web3 = window.web3;
+    // Load account
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
+    // Network ID
+    const networkId = await web3.eth.net.getId();
+    const networkData = SocialNetwork.networks[networkId];
+    if (networkData) {
+      const socialNetwork = new web3.eth.Contract(SocialNetwork.abi, networkData.address);
+      setSocialNetwork(socialNetwork);
+      const postCount = await socialNetwork.methods.postCount().call();
+      setPostCount(postCount);
+      // Load Posts
+      const posts = [];
+      for (let i = 1; i <= postCount; i++) {
+        const post = await socialNetwork.methods.posts(i).call();
+        posts.push(post);
+      }
+      setPosts([...posts]);
+      setLoading(false);
+    } else {
+      window.alert('SocialNetwork contract not deployed to detected network.');
+    }
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      await loadWeb3();
+      await loadBlockchainData();
+    }
+    fetchData();
+  }, []);
+
+  const createPost = (content) => {
+    setLoading(true);
+    socialNetwork.methods.createPost(content).send({ from: account })
+      .once('receipt', (receipt) => {
+        setLoading(false);
+      })
+  }
+
   return (
     <div>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <div className="container-fluid">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 me-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </div>
-      </nav>
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
-            <div className="content ms-auto me-auto">
-              <a
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={logo} className="App-logo" alt="logo" />
-              </a>
-              <h1>Dapp University Starter Kit</h1>
-              <p>
-                Edit <code>src/components/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-              </a>
-            </div>
-          </main>
-        </div>
-      </div>
+      <Navbar account={account} />
+      { loading
+        ? <div id="loading" className="text-center mt-5"><p>Loading...</p></div>
+        : <Main
+            posts={posts}
+            createPost={createPost}
+          />
+      }
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,70 @@
+import { useRef } from 'react';
+import Identicon from 'identicon.js';
+
+function Main({ posts, createPost }) {
+  const postContentInput = useRef(null);
+
+  const handleSubmitContent = (e) => {
+    e.preventDefault();
+    createPost(postContentInput.current.value);
+  }
+
+  return (
+    <div className="container-fluid mt-5">
+      <div className="row">
+        <main role="main" className="col-lg-12 mx-auto" style={{ maxWidth: '500px' }}>
+          <div className="content ms-auto me-auto">
+            <p>&nbsp;</p>
+            <form className="d-grid" onSubmit={handleSubmitContent}>
+              <div className="form-group mr-sm-2">
+                <input
+                  id="postContent"
+                  type="text"
+                  ref={postContentInput}
+                  className="form-control"
+                  placeholder="What's on your mind?"
+                  required
+                />
+              </div>
+              <button type="submit" className="btn btn-primary btn-block mt-2">Share</button>
+            </form>
+            <p>&nbsp;</p>
+            { posts.map((post, key) => {
+              return (
+                <div className="card mb-4" key={key} >
+                  <div className="card-header">
+                    <img
+                      className="me-2"
+                      width="30"
+                      height="30"
+                      src={`data:image/png;base64,${new Identicon(post.author, 30).toString()}`}
+                    />
+                    <small className="text-muted">{post.author}</small>
+                  </div>
+                  <ul id="postList" className="list-group list-group-flush">
+                    <li className="list-group-item">
+                      <p>{post.content}</p>
+                    </li>
+                    <li key={key} className="list-group-item py-2">
+                      <small className="float-start mt-1 text-muted">
+                        TIPS: {window.web3.utils.fromWei(post.tipAmount.toString(), 'ether')} ETH
+                      </small>
+                      <button
+                        className="btn btn-link btn-sm float-end pt-0"
+                        name={post.id}
+                      >
+                        TIP 0.1 ETH
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              );
+            })}
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
