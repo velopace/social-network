f0955a8753a4164829a9dee4899ea2ddc51ecd73
velopace <velopace@outlook.com>
2021-08-10 11:19:05 -0500

    Create post and test

--- test/SocialNetwork.test.js
+++ test/SocialNetwork.test.js
@@ -6,7 +6,7 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-contract('SocialNetwork', (accounts) => {
+contract('SocialNetwork', ([deployer, author, tipper]) => {
   let socialNetwork
 
   before(async () => {
@@ -29,8 +29,22 @@ contract('SocialNetwork', (accounts) => {
   })
 
   describe('posts', () => {
-    it('creates posts', async () => {
+    let result, postCount
 
+    it('creates posts', async () => {
+      result = await socialNetwork.createPost('This is my first post', { from: author })
+      postCount = await socialNetwork.postCount()
+
+      // Success
+      assert.equal(postCount, 1)
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct')
+      assert.equal(event.content, 'This is my first post', 'content is correct')
+      assert.equal(event.tipAmount, '0', 'tip amount is correct')
+      assert.equal(event.author, author, 'author is correct')
+
+      // Failure: Post must have a content
+      await socialNetwork.createPost('', { from: author }).should.be.rejected;
     })
 
     it('lists posts', async () => {
